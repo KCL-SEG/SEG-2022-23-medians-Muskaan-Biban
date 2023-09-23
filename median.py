6e0373b26d002c354adeacd9735937035c205230
k22037403 <k22037403@kcl.ac.uk>
2023-09-23 13:12:44 +0100

    final commit

--- median.py
+++ median.py
@@ -4,9 +4,17 @@
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        numbers = [int(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+    
+numbers.sort()
+    
+if len(numbers) % 2 != 0:
+    print(numbers[len(numbers) // 2])
+else:
+    first = numbers[((len(numbers) // 2) - 1)]
+    second = numbers[len(numbers) // 2]
+    print((first + second) / 2)
